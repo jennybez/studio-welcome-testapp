feb8cd8d8cabd371664d78b1a4ae3a6606496041
Pe4enbka <72940882+Pe4enbka@users.noreply.github.com>
2021-06-19 11:52:29 +0400

    Убрана почта Дани (вместо нее mail@mail.mail)

--- send.php
+++ send.php
@@ -7,20 +7,16 @@ $mail->CharSet = 'utf-8';
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  						// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dperfilov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '100158'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'mail@mail.mail'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'mail@mail.mail'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dperfilov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('danil1601perfilov@gmail.com');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+$mail->setFrom('mail@mail.mail'); // от кого будет уходить письмо?
+$mail->addAddress('mail@mail.mail');     // Кому будет уходить письмо 
+
 $mail->addAttachment($_FILES['upload']['tmp_name'], $_FILES['upload']['name']);    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
